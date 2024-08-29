bb7118743cd762fc402d50748ebe3030b2b3ca46
Ataliba Paulo Chilela Miguel <112531858+achilela@users.noreply.github.com>
2024-08-29 17:35:15 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import matplotlib.pyplot as plt
 import matplotlib.patches as patches
+
+
+
+# Function to draw straight rectangles without chamfer
+def add_rectangle(ax, xy, width, height, **kwargs):
+    rectangle = patches.Rectangle(xy, width, height, **kwargs)
+    ax.add_patch(rectangle)
  
 # Function to draw chamfered rectangles
 def add_chamfered_rectangle(ax, xy, width, height, chamfer, **kwargs):
@@ -25,26 +32,30 @@ st.title('CLV')
  
 # Define the layout modules and positions
 modules = {
-    'M120': (0.5, 1), 'M121': (0.5, 2), 'M122': (0.5, 3), 'M123': (0.5, 4),
-    'M124': (0.5, 5), 'M125': (0.5, 6), 'M126': (0.5, 7), 'M110': (2, 1),
-    'M111': (2, 2), 'M112': (2, 3), 'M113': (2, 4), 'M114': (2, 5),
-    'M115': (2, 6), 'M116': (2, 7)
+    'M120': (0.5, 2), 'M121': (0.5, 3), 'M122': (0.5, 4), 'M123': (0.5, 5),
+    'M124': (0.5, 6), 'M125': (0.5, 7), 'M126': (0.5, 8), 'M110': (2, 2),
+    'M111': (2, 3), 'M112': (2, 4), 'M113': (2, 5), 'M114': (2, 6),
+    'M115': (2, 7), 'M116': (2, 8)
 }
  
 racks = {
-    'P-RACK 141': (1.5, 1), 'P-RACK 142': (1.5, 2), 'P-RACK 143': (1.5, 3),
-    'P-RACK 144': (1.5, 4), 'P-RACK 145': (1.5, 5), 'P-RACK 146': (1.5, 6)
+    'P-RACK 141': (1.5, 2), 'P-RACK 142': (1.5, 3), 'P-RACK 143': (1.5, 4),
+    'P-RACK 144': (1.5, 5), 'P-RACK 145': (1.5, 6), 'P-RACK 146': (1.5, 7)
 }
 
 flare = {
-     'FLARE': (0.5, 8) 
+     'FLARE': (0.5, 9) 
 }
+
+living_quarter = {
+               'LQ': (0.5, 1)
+ }
  
 # Create a figure and axis for the layout
 fig, ax = plt.subplots(figsize=(12, 8))
  
 # Set the axis limits and aspect ratio
-ax.set_xlim(0, 10)
+ax.set_xlim(0, 12)
 ax.set_ylim(0, 3.5)
 ax.set_aspect('equal')
  
@@ -59,10 +70,6 @@ for rack, (row, col) in racks.items():
     add_chamfered_rectangle(ax, (col, row), width, 0.5, 0.05, edgecolor='black', facecolor='white')
     ax.text(col + width / 2, row + 0.25, rack, ha='center', va='center', fontsize=8)
 
-# Draw the flare with chamfer only at the top
-#for flare, (row, col) in flare.items():
-#    add_chamfered_rectangle(ax, (col, row), 1, 2.5, 0.1, edgecolor='black', facecolor='white')
-#    ax.text(col + 0.5, row + 1.25, flare, ha='center', va='center', fontsize=10)
 
 # Draw the flare with chamfer only at the top
 for flare, (row, col) in flare.items():
