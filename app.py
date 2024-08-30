b6336f38aab0c37a28fa999381550cad73bc0097
Ataliba Paulo Chilela Miguel <112531858+achilela@users.noreply.github.com>
2024-08-30 16:32:14 +0100

    Update app.py

--- app.py
+++ app.py
@@ -99,7 +99,7 @@ ax.set_aspect('equal')
 # Draw the M modules
 for module, (row, col) in modules.items():
     width = 1.75 if module == 'M110' else 1
-    add_chamfered_rectangle(ax, (col, row), 1, 1.75, 0.1, edgecolor='black', facecolor='white')
+    add_chamfered_rectangle(ax, (col, row), 1, 1, 0.1, edgecolor='black', facecolor='white')
     ax.text(col + 0.5, row + 0.5, module, ha='center', va='center', fontsize=7)
 
 # Draw the RACK modules
